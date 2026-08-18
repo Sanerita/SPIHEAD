0570fdd94e289c0a3e35181d7635c0afc9c14a9b
Sanelisiwe Sileku <sanelisiwe.sileku@gmail.com>
2026-08-18 23:04:18 +0200

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
+// drizzle.config.ts
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/db/schema.ts',
   out: './drizzle',
+  schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL || '',
+    url: process.env.DATABASE_URL!,
   },
 });
